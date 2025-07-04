b5ce94922f6a143e8b7a0a195334dc5901fea4e9
Abdo <abdo@abdnh.net>
2025-07-04 09:36:22 +0300

    Patch certifi.where()

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 from aqt import mw
 from aqt.qt import QAction, QMenu, qconnect
 
+from .patches import patch_certifi
+
+patch_certifi()
+
+# ruff: noqa: E402
 from . import editor, web
 from .consts import consts
 from .errors import setup_error_handler
@@ -24,4 +29,5 @@ def init() -> None:
     setup_error_handler()
     editor.init_hooks()
     web.init_hooks()
+    add_menu()
     add_menu()
